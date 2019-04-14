5f3f7025c5ed882db7e8a77c785c37c2fec03cf0
Manjnth <manjuanth.kadlimatti@nobroker.in>
2019-04-14 17:12:48 +0530

    sw changes

--- main.js
+++ main.js
@@ -12,7 +12,7 @@
 if('serviceWorker' in navigator){
   window.addEventListener('load', ()=> {
   navigator.serviceWorker
-  .register('../sw_cached_pages.js')
+  .register('sw_cached_site.js')
   .then(req => console.log('Service Worker: Registered', req))   
   .catch(err => console.log(`service worker error ${err}`))
   })
